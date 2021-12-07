c6eb9c26727967531a570ebabdd9dbf65404ebd6
snoop2head <young_ahn@yonsei.ac.kr>
2021-12-07 09:00:42 +0000

    update: vgg11_elim_2 (temporary replacement for vgg11)

--- src/modules/vgg11.py
+++ src/modules/vgg11.py
@@ -17,11 +17,39 @@ class VGG11(nn.Module):
 
         # conditional elimination on convolutional layers based on desired output channel
         if out_channel == 512:
+            self.model.features = self.model.features[:16]
+            # Sequential(
+            #     (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            #     (1): ReLU(inplace=True)
+            #     (2): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            #     (3): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            #     (4): ReLU(inplace=True)
+            #     (5): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            #     (6): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            #     (7): ReLU(inplace=True)
+            #     (8): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            #     (9): ReLU(inplace=True)
+            #     (10): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            #     (11): Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            #     (12): ReLU(inplace=True)
+            #     (13): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            #     (14): ReLU(inplace=True)
+            #     (15): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            # )
             pass
         elif out_channel == 256:
-            pass
-        elif out_channel == 128:
-            pass
+            self.model.features = self.model.features[:11]
+            # (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            # (1): ReLU(inplace=True)
+            # (2): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            # (3): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            # (4): ReLU(inplace=True)
+            # (5): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            # (6): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            # (7): ReLU(inplace=True)
+            # (8): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            # (9): ReLU(inplace=True)
+            # (10): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
 
         del self.model.avgpool
         del self.model.classifier
